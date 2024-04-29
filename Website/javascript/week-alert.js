0557ff844f0a6e79f579df7f2c4399fedf0cc787
Mostafa-El-gelany <Mostafa-El-gelany>
2024-04-29 20:55:35 +0300

    update notifcation hover

--- Website/javascript/week-alert.js
+++ Website/javascript/week-alert.js
@@ -1,6 +1,39 @@
 
 
 
+function appendNotification( mainTitle, description, time, link) {
+    const parentElement = document.getElementById("week-alerts-tab-parant");
+    const aElement = document.createElement('a');
+    aElement.href = link;
+    aElement.classList.add('list-group-item', 'list-group-item-action', 'flex-column', 'align-items-start');
+    
+    const divElement = document.createElement('div');
+    divElement.classList.add('d-flex', 'w-100', 'justify-content-between');
+    
+    const h5Element = document.createElement('h5');
+    h5Element.classList.add('mb-1');
+    h5Element.textContent = mainTitle;
+    
+    const smallElement = document.createElement('small');
+    smallElement.textContent = time;
+    
+    divElement.appendChild(h5Element);
+    divElement.appendChild(smallElement);
+    
+    const pElement = document.createElement('p');
+    pElement.classList.add('mb-1');
+    pElement.textContent = description;
+    
+    const smallElement2 = document.createElement('small');
+    smallElement2.textContent = 'By : Mohamed mostafa';
+    
+    aElement.appendChild(divElement);
+    aElement.appendChild(pElement);
+    aElement.appendChild(smallElement2);
+    
+    parentElement.appendChild(aElement);
+}
+
 
 var notifications = [
     {
@@ -8,62 +41,77 @@ var notifications = [
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/profile1.png',
-        readed : false
+        readed : false,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material 3',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/logo.png',
-        readed : false
+        readed : false,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/profile1.png',
-        readed : true
+        readed : true,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/profile1.png',
-        readed : false
+        readed : false,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course this is lorem ipsum text to test the text overflow in the notification card and this is the end of the text New material has been added to the course this is lorem ipsum text to test the text overflow in the notification card and this is the end of the text New material has been added to the course this is lorem ipsum text to test the text overflow in the notification card and this is the end of the text', 
         date: '2 h',
         imgSrc: '../assets/images/profile2.png',
-        readed : true
+        readed : true,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/profile2.png',
-        readed : false
+        readed : false,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/logo.png',
-        readed : true
+        readed : true,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
+
     },
     {
         mainTitle: 'New Material',
         description: 'New material has been added to the course',
         date: '2 h',
         imgSrc: '../assets/images/profile1.png',
-        readed : false
+        readed : false,
+        URl : "http://127.0.0.1:5500/html/material.html?id=1",
     }
 ];
 
 
-
-for (let i = 0; i < notifications.length; i++) {
-    const notification = notifications[i];
-    appendNotification("pills-contact", notification.mainTitle, notification.description, notification.date, notification.imgSrc);
+notifications.forEach(notification => {
+    appendNotification( notification.mainTitle, notification.description, notification.date,"http://127.0.0.1:5500/html/material.html?id=1");
 }
+);
+
