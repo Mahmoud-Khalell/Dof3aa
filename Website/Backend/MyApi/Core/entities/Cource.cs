2713130f220d56771c2ac0c4ecd419b95bd885c1
khalil <mahmoud.khalil.luxor.edu.eg>
2024-05-03 19:35:08 +0300

    update cors

--- Website/Backend/MyApi/Core/entities/Cource.cs
+++ Website/Backend/MyApi/Core/entities/Cource.cs
@@ -8,6 +8,6 @@ namespace Core.entities
 {
     public class Cource
     {
-
+        
     }
 }
--- Website/Backend/MyApi/Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
+++ Website/Backend/MyApi/Core/obj/Debug/net6.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f069cce5a23188672be475bb902aa283884a29e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5bdbf527fc3851768149bfc9ca7766900395b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
