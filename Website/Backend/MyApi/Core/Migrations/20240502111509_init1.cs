f069cce5a23188672be475bb902aa283884a29e8
khalil <mahmoud.khalil.luxor.edu.eg>
2024-05-03 15:32:38 +0300

    backend add Users

---
+++ Website/Backend/MyApi/Core/Migrations/20240502111509_init1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    code = table.Column<int>(type: "int", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courceemployee",
+                columns: table => new
+                {
+                    CourcesId = table.Column<int>(type: "int", nullable: false),
+                    employeesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courceemployee", x => new { x.CourcesId, x.employeesId });
+                    table.ForeignKey(
+                        name: "FK_Courceemployee_Cources_CourcesId",
+                        column: x => x.CourcesId,
+                        principalTable: "Cources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Courceemployee_employees_employeesId",
+                        column: x => x.employeesId,
+                        principalTable: "employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courceemployee_employeesId",
+                table: "Courceemployee",
+                column: "employeesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courceemployee");
+
+            migrationBuilder.DropTable(
+                name: "Cources");
+        }
+    }
+}
---
+++ Website/Backend/MyApi/MyApi/Model/Repositories/Generic.cs
@@ -0,0 +1,43 @@
+﻿using Core.Context;
+using Core.entities;
+using MyApi.Model.Interfaces;
+
+namespace MyApi.Model.Repositories
+{
+    public class Generic<T> : IGeneric<T> where T : BaseEntity
+    {
+        private readonly Connector connector;
+
+        public Generic(Connector connector) 
+        {
+            this.connector = connector;
+        }
+        public int add(T entity)
+        {
+            connector.Set<T>().Add(entity);
+            return connector.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+           return connector.Set<T>().ToList();
+        }
+
+        public T GetById(int? id)
+        {
+            return connector.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+
+        public int remove(T entity)
+        {
+            connector.Set<T>().Remove(entity);
+            return connector.SaveChanges();
+        }
+
+        public int update(T entity)
+        {
+            connector.Set<T>().Update(entity);
+            return connector.SaveChanges();
+        }
+    }
+}
