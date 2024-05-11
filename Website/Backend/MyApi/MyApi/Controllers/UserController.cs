4662a29cb5ef55e8d6ef00eab4d2febff22e8d68
khalil <mahmoud.khalil.luxor.edu.eg>
2024-05-11 16:38:12 +0300

    add password update

--- Website/Backend/MyApi/MyApi/Controllers/UserController.cs
+++ Website/Backend/MyApi/MyApi/Controllers/UserController.cs
@@ -13,6 +13,8 @@ using MyApi.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Cors;
 using MyApi.Model.Repositories;
+using MimeKit;
+using MailKit;
 namespace MyApi.Controllers
 {
     [Route("api/[controller]")]
@@ -20,10 +22,12 @@ namespace MyApi.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUnitOfCode unit;
+        private readonly IWebHostEnvironment webHost;
 
-        public UserController(IUnitOfCode unit)
+        public UserController(IUnitOfCode unit,IWebHostEnvironment webHost)
         {
             this.unit = unit;
+            this.webHost = webHost;
         }
 
 
@@ -51,17 +55,59 @@ namespace MyApi.Controllers
             var token = await unit.UserManager.GenerateEmailConfirmationTokenAsync(user);
             // crete link that lead to endpoint ConfirmEmail 
             var link = Url.Action("ConfirmEmail", "User", new { email = email, token = token }, Request.Scheme, Request.Host.ToString());
-            Email Reciever = new Email()
-            {
-                To = email,
-                Title = "Confirm Email",
-                Body = link
-            };
-            EmailService.SendEmail(Reciever);
+            
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Dof3aa","icpcfinalist@gmail.com"));
+            message.To.Add(new MailboxAddress("",email));
+            message.Subject = "Confirm Email";
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = EmailService.GetEmailBodyForEmailConfirm(link); 
+            message.Body = builder.ToMessageBody();
+            EmailService.SendEmail(message);
             return Ok();
 
 
         }
+        #endregion
+
+        #region Update Password
+        [HttpGet("UpdatePassword")]
+        public async Task<IActionResult> ForgetPassword(string email)
+        {
+            var user = unit.UserManager.Users.FirstOrDefault(e => e.Email == email);
+            if (user == null)
+                return NotFound();
+            var token = await unit.UserManager.GeneratePasswordResetTokenAsync(user);
+            // crete link that lead to endpoint ConfirmEmail 
+            
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Dof3aa", "icpcfinalist@gmail.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = "Token for updateing Password";
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = EmailService.GetEmailBodyForPasswordReset(token);
+            message.Body = builder.ToMessageBody();
+            EmailService.SendEmail(message);
+            return Ok();
+        }
+
+        #endregion
+
+        #region Update Password Confirm
+        [HttpGet("UpdatePasswordConfirm")]
+        public async Task<IActionResult> UpdatePasswordConfirm(string email, string token, string newPassword)
+        {
+            var user = unit.UserManager.Users.FirstOrDefault(e => e.Email == email);
+            if (user == null)
+                return NotFound();
+            var result = await unit.UserManager.ResetPasswordAsync(user, token, newPassword);
+            if (result.Succeeded)
+                return Ok();
+            return BadRequest(result.Errors.FirstOrDefault());
+        }
+
         #endregion
 
         #region Register
@@ -143,13 +189,12 @@ namespace MyApi.Controllers
             return Ok(US.rule);
 
         }
-<<<<<<< HEAD
         #endregion
-=======
-        
->>>>>>> 00cf06d7ae8e5687ae9aaf56081cae36b7a9d54f
 
         
+
+
+
 
 
     }
